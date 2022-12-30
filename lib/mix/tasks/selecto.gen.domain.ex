fba79b1ca18acbec14b6f7dced1ec12cd193b9d9
Chris Rohlfs <seeken@gmail.com>
2022-12-29 21:43:12 -0700

    stub accept opts

--- lib/mix/tasks/selecto.gen.domain.ex
+++ lib/mix/tasks/selecto.gen.domain.ex
@@ -2,9 +2,14 @@ defmodule Mix.Tasks.Selecto.Gen.Domain do
   @moduledoc "The hello mix task: `mix help hello`"
   use Mix.Task
 
-  @shortdoc "Simply calls the Hello.say/0 function."
-  def run(_) do
+  @shortdoc "Generate a Selecto Domain module."
+  def run(args) do
     # calling our Hello.say() function from earlier
     IO.puts("HERE")
+
+    {_parsed, rest} = OptionParser.parse!(args)
+
+    IO.inspect( rest )
+
   end
 end
